7b809aa99b8b48e1f34fc256a5c8a95a05e0fb44
Hanieh <119368538+haniehsadgh@users.noreply.github.com>
2024-04-02 19:31:20 -0700

    commented the import line

--- storage/models_mysql.py
+++ storage/models_mysql.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, DateTime, func
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import DeclarativeBase, mapped_column
+#from sqlalchemy.orm import DeclarativeBase, mapped_column
 from sqlalchemy import Integer, String, DateTime, func
 import datetime
 
