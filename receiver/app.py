11411a0c7874f4c1d919db54287c1de186d27533
Hanieh <119368538+haniehsadgh@users.noreply.github.com>
2024-04-04 14:30:44 -0700

    fixed indentation in in with reading log files

--- receiver/app.py
+++ receiver/app.py
@@ -18,8 +18,10 @@ import yaml
 MAX_EVENTS = 5
 EVENT_FILE = 'events.json'
 
+
 with open('app_conf.yml', 'r') as f:
     app_config = yaml.safe_load(f.read())
+
 
 with open('log_conf.yml', 'r') as f:
     log_config = yaml.safe_load(f.read())
