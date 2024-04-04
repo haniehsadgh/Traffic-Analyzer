8ef2de76c8db0c0062429bbe558270d9394e56d0
Hanieh <119368538+haniehsadgh@users.noreply.github.com>
2024-04-04 14:07:20 -0700

    fixed indentation

--- receiver/app.py
+++ receiver/app.py
@@ -14,11 +14,11 @@ MAX_EVENTS = 5
 EVENT_FILE = 'events.json'
 
 with open('app_conf.yml', 'r') as f:
- app_config = yaml.safe_load(f.read())
+    app_config = yaml.safe_load(f.read())
 
 with open('log_conf.yml', 'r') as f:
- log_config = yaml.safe_load(f.read())
- logging.config.dictConfig(log_config)
+    log_config = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_config)
 
 logger = logging.getLogger('basicLogger')
 
