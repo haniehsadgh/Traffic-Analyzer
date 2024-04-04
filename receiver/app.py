272800cbd17b6e22fb35756f97fb83972402c30d
Hanieh <119368538+haniehsadgh@users.noreply.github.com>
2024-04-04 14:03:27 -0700

    ordered the imports

--- receiver/app.py
+++ receiver/app.py
@@ -1,23 +1,20 @@
-import time
-from datetime import datetime 
 import json
+import logging
+import logging.config
+import uuid
+from datetime import datetime
+from os import path
+
 import connexion
 from connexion import NoContent
-from flask import Flask, request, jsonify
-import requests
-from os import path
-import yaml
-import logging
-import uuid
-import logging.config
 from pykafka import KafkaClient
+import yaml
 
 MAX_EVENTS = 5
 EVENT_FILE = 'events.json'
 
 with open('app_conf.yml', 'r') as f:
  app_config = yaml.safe_load(f.read())
-
 
 with open('log_conf.yml', 'r') as f:
  log_config = yaml.safe_load(f.read())
@@ -27,7 +24,6 @@ logger = logging.getLogger('basicLogger')
 
 def generate_trace_id():
     return str(uuid.uuid4())
-
 
 
 def recordTrafficFlow(body):
@@ -52,7 +48,6 @@ def recordTrafficFlow(body):
     return NoContent, 201
 
     
-
 def reportIncident(body):
     trace_id = generate_trace_id()
     logger.info(f"Recieved event {app_config['accident']} request with a trace id of {trace_id})")
@@ -70,11 +65,9 @@ def reportIncident(body):
             "payload": body }
     msg_str = json.dumps(msg)
     producer.produce(msg_str.encode('utf-8'))
-    
 
     logger.info(f"Returned event {app_config['accident']} response (Id: {trace_id}) with status 201")
     return NoContent, 201
-
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
@@ -82,4 +75,3 @@ app.add_api("trafficreport.yaml", strict_validation=True, validate_responses=Tru
 
 if __name__ == "__main__":
     app.run(port=8080)
-
