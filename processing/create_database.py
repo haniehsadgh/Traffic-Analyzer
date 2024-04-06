03ae3bce3a7e4e30abafdb462cfd22835ba305d0
Hanieh <119368538+haniehsadgh@users.noreply.github.com>
2024-04-05 21:06:40 -0700

    added docstring

--- processing/create_database.py
+++ processing/create_database.py
@@ -1,11 +1,21 @@
+"""
+Module to create and drop database tables.
+"""
+
 from models import Base
 from db import engine
 
 def create_tables():
+    """
+    Create all tables defined in the Base metadata.
+    """
     Base.metadata.create_all(engine)
 
 
 def drop_tables():
+    """
+    Drop all tables defined in the Base metadata.
+    """
     Base.metadata.drop_all(engine)
 
 
